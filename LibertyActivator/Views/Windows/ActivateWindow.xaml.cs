fe7d28db998316d7705e03f51816496543af0661
Илья Гниденко <gn.ilia@yandex.ru>
2025-06-23 23:17:09 +0500

    add HeaderPanel

--- LibertyActivator/Views/Windows/ActivateWindow.xaml.cs
+++ LibertyActivator/Views/Windows/ActivateWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace LibertyActivator.Views.Windows
 {
@@ -10,6 +11,22 @@ namespace LibertyActivator.Views.Windows
 		public ActivateWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void HeaderPanel_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			this.DragMove();
+		}
+
+		private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.WindowState = WindowState.Minimized;
+		}
+
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			base.Close();
+			Environment.Exit(0);
 		}
 	}
 }
